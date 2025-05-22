358cc94abbdcb4fc4fe337367f8a5047f4438c5c
Syoitu Den <tian.xiaoyi@nint.jp>
2025-05-22 11:39:56 +0900

    Initial commit

---
+++ test/cdk-image-upload.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkImageUpload from '../lib/cdk-image-upload-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-image-upload-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkImageUpload.CdkImageUploadStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
